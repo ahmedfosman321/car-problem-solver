1a811fb6effc4e383a8595c426349aa37e5fa834
ahmedfosman321 <apo5245@psu.edu>
2026-04-01 21:40:41 -0400

    Delete drivediagnose directory

--- drivediagnose/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    // Allow external image domains if needed in the future
-    domains: [],
-  },
-};
-
-module.exports = nextConfig;
